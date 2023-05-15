032bcab3919f9f403f720a8371e2d5d0d934ecda
BhagyashriSabale <balisabale16@gmail.com>
2023-05-15 22:02:42 +0530

    [Bhagyashri] welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HashTablesandBST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Hashtable and BST Programming");
+        }
+    }
+}
